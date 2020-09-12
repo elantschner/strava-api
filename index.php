9eac6ae40dc3b145f3373146e523e260b55b4210
LOEX\elantschner <elantschner@live.com>
2020-09-12 18:27:59 +0200

    index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strava</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
 </head>
 <body>
     <h1>My STRAVA-App</h1>
@@ -20,5 +22,17 @@
         <li><a href="api/v3/athlete.php">ATHLETE</a></li>
         <li><a href="api/v3/activities.php">ACTIVITIES</a></li>
     </ul>
+
+    <div>
+        <?php
+            $data = json_decode($_COOKIE["authorization"], true);
+            echo "<h2>".$data["athlete"]["firstname"]." ".$data["athlete"]["lastname"]."</h2>";
+
+            echo "<pre>";
+            var_dump($data);
+            echo "</pre>"
+        ?>   
+    </div> 
+    
 </body>
 </html>
